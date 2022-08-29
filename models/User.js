36a80202056ba211995a5529df3abb71712b3bf4
EnrisKumi <107574553+EnrisKumi@users.noreply.github.com>
2022-08-29 16:15:03 +0200

    User Model

--- models/User.js
+++ models/User.js
@@ -1,18 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    location: {
+      type: String,
+    },
+    bio: {
+      type: String,
+    },
+    followed: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    followers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timeStamps: true,
+  });
 
-const UserSchema = new mongoose.Schema({
-
-    firstName: {
-        type : String,
-        required : true,
-        unique: true
-    }
-})
-
-module.exports = mongoose.model('User', UserSchema);
-
-/*{
-        timeStamps: true
-    })*/
+module.exports = mongoose.model("User", UserSchema);
