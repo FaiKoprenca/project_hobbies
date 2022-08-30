8c2dfb7faf139c9752e936801c2278ffbc5376dd
Fane Veizi <faneveizi@gmail.com>
2022-08-30 20:28:42 +0200

    Fixed postCommentAtPost route

--- handlers/handlerTest.js
+++ handlers/handlerTest.js
@@ -93,7 +93,7 @@ module.exports.postCommentAtPost = async (event, context, callback) => {
         const createComment = await Comment.create(comments);
         const createdComment = await Post.findOneAndUpdate(
             { _id: id },
-            { $push: { comments: createComment._id } },
+            { $push: { comment: createComment._id } },
             { new: true }
         );
         callback(null, {
