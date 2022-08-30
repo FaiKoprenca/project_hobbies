443de467ae40839a1edb5325a3c91a6dcf74bb13
EnrisKumi <107574553+EnrisKumi@users.noreply.github.com>
2022-08-30 13:00:02 +0200

    update post model

--- models/Post.js
+++ models/Post.js
@@ -1,31 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
-
+const PostSchema = new Schema(
+  {
     userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
     username: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     text: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    tags: [{
-        type: String
-    }],
+    tags: [
+      {
+        type: String,
+      },
+    ],
     date: {
-        type: Date,
-        default: Date.now
-      }
+      type: Date,
+      default: Date.now,
+    },
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+    },
+    
+    limit: {
+      type: Number,
+      required: true,
+    },
+    history: [
+      {
+        date: { type: Date, default: Date.now() },
+        userId: { type: Schema.Types.ObjectId, ref: "User" },
+      },
+    ],//front end type of event: indor or outdor
+  },
+  {
+    timeStamps: true,
+  }
+);
 
-},
-{
-    timeStamps: true
-})
-
-module.exports = mongoose.model('Post', PostSchema);
+module.exports = mongoose.model("Post", PostSchema);
