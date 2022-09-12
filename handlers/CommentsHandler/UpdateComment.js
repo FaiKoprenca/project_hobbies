25b99533b5adff9f4ca8e3ccca46692ec492f5ed
FaiKoprenca <88925964+FaiKoprenca@users.noreply.github.com>
2022-09-12 13:35:22 +0200

    updateComments fix

--- handlers/CommentsHandler/UpdateComment.js
+++ handlers/CommentsHandler/UpdateComment.js
@@ -4,37 +4,31 @@ const Comment = require("../../models/Comments");
 
 module.exports.updateComment = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  const data = JSON.parse(event.body);
-  //TODO
-  // const { errors, isValid } = validateCommentInput(event.body);
-
-  // if (!isValid) {
-  //   return res.status(400).json(errors);
-  // }
-
-  const { text } = data;
 
   try {
     await connectToDatabase();
 
-    const comment = await Comment.findById(event.pathParameters.id);
+    const reqBody = JSON.parse(event.body);
 
-    if (comment) {
-      comment.text = text || comment.text;
-    }
-
-    const newComment = await comment.save();
+    const updateComment = await Comment.updateOne(
+      {
+        _id: event.pathParameters.id,
+      },
+      {
+        text: reqBody.text,
+      }
+    )
 
     return {
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
-        Allow: "GET, OPTIONS, POST",
+        "Allow": "GET, OPTIONS, POST",
         "Access-Control-Allow-Methods": "GET, OPTIONS, POST",
         "Access-Control-Allow-Headers": "*",
       },
       statusCode: 200,
-      body: JSON.stringify(newComment),
+      body: JSON.stringify(updateComment),
     };
   } catch (error) {
     return(error);
