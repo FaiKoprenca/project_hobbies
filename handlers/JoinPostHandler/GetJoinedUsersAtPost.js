d358b02813d36febdf0cf33eb179d7d67def568a
FaiKoprenca <88925964+FaiKoprenca@users.noreply.github.com>
2022-09-14 12:01:05 +0200

    Join Post Handler

---
+++ handlers/JoinPostHandler/GetJoinedUsersAtPost.js
@@ -0,0 +1,6 @@
+const connectToDatabase = require("../../database/db");
+const User = require("../../models/User");
+const Post = require("../../models/Post");
+const { models } = require("mongoose");
+
+//models.exports.getJoined
