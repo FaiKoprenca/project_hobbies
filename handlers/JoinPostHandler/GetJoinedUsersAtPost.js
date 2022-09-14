bb75efcfa88ce72e9347539f4ae7f79fba264f24
FaiKoprenca <88925964+FaiKoprenca@users.noreply.github.com>
2022-09-14 12:57:53 +0200

    Populate Joined At PoST

--- handlers/JoinPostHandler/GetJoinedUsersAtPost.js
+++
@@ -1,6 +0,0 @@
-const connectToDatabase = require("../../database/db");
-const User = require("../../models/User");
-const Post = require("../../models/Post");
-const { models } = require("mongoose");
-
-//models.exports.getJoined
