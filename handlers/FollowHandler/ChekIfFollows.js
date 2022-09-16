5f5ab4016433fee70d4c3d635d8f6c663e0679dd
FaiKoprenca <88925964+FaiKoprenca@users.noreply.github.com>
2022-09-16 14:13:57 +0200

    Check If user has userX follow

---
+++ handlers/FollowHandler/ChekIfFollows.js
@@ -0,0 +1,43 @@
+const connectToDatabase = require("../../database/db");
+const User = require("../../models/User");
+
+module.exports.checkIfFollows = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const id = event.pathParameters.id;
+  const followersId = event.pathParameters.followersId;
+
+  try {
+    await connectToDatabase();
+
+    
+
+    const userFollower = await User.findById(followersId);
+
+    const boolean = userFollower.followers.includes(id)
+    
+    // for (let index = 0; index < userFollower.followers.length; index++) {
+    //     if (userFollower.followers[index]=== id) {
+    //         boolean = true 
+            
+    //     }
+        
+    // }
+    console.log(boolean);
+    
+   
+    callback(null, {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Allow": "GET, OPTIONS, POST",
+        "Access-Control-Allow-Methods": "GET, OPTIONS, POST",
+        "Access-Control-Allow-Headers": "*",
+      },
+      statusCode: 200,
+      body: JSON.stringify(boolean),
+    });
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
