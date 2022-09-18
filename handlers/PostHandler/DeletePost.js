9cec78764bb4bda1ea09c1906ea20423386357e5
FaiKoprenca <88925964+FaiKoprenca@users.noreply.github.com>
2022-09-18 18:02:45 +0200

    delete post fix

--- handlers/PostHandler/DeletePost.js
+++ handlers/PostHandler/DeletePost.js
@@ -3,31 +3,35 @@ const User = require('../../models/User');
 const Post = require('../../models/Post');
 
 
-
 module.exports.deletePost = async (event, context, callback) => {
-    context.callbackWaitsForEmptyEventLoop = false;
-    const id = event.pathParameters.id;
-  
-    try {
-      await connectToDatabase();
-      const post = await Post.findByIdAndRemove(id);
-      if (!post) {
-        callback(null, (404, `No post found with id: ${id}, cannot delete`));
-      }
-      return {
-        headers: {
-          "Content-Type" : "application/json",
-          "Access-Control-Allow-Origin" : "*",
-          "Allow" : "GET, OPTIONS, POST",
-          "Access-Control-Allow-Methods" : "GET, OPTIONS, POST",
-          "Access-Control-Allow-Headers" : "*"
+  context.callbackWaitsForEmptyEventLoop = false;
+  const userId = event.pathParameters.userId;
+  const postId = event.pathParameters.postId;
+
+
+  try {
+    await connectToDatabase();
+    const user = await User.findOneAndUpdate(
+      { _id: userId },
+      { $pull: { posts: postId } },
+      { new: true },
+    )
+    if (!user) {
+      callback(null, (404, `No post found with id: ${userId}, cannot delete`));
+    }
+
+    return {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Allow": "GET, OPTIONS, POST",
+        "Access-Control-Allow-Methods": "GET, OPTIONS, POST",
+        "Access-Control-Allow-Headers": "*"
       },
-        statusCode: 200,
-        body: JSON.stringify({
-          message: `Removed post with id: ${post._id}`,
-          post,
-        }),
-      };
-    } catch (error) {
-      return(error);
-}};
+      statusCode: 200,
+      body: JSON.stringify("!!!!!!!!!!!!!!!DONE!!!!!!!!!!!!!!!!!!!"),
+    };
+  } catch (error) {
+    return (error);
+  }
+};
