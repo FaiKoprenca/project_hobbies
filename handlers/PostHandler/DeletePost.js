36947d7d9cbd236ae0a3001d4ede5768b62db593
FaiKoprenca <88925964+FaiKoprenca@users.noreply.github.com>
2022-09-18 18:24:40 +0200

    new function for deleting post

--- handlers/PostHandler/DeletePost.js
+++ handlers/PostHandler/DeletePost.js
@@ -5,33 +5,33 @@ const Post = require('../../models/Post');
 
 module.exports.deletePost = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  const userId = event.pathParameters.userId;
-  const postId = event.pathParameters.postId;
+  const id = event.pathParameters.id;
+  //const postId = event.pathParameters.postId;
 
 
-  try {
-    await connectToDatabase();
-    const user = await User.findOneAndUpdate(
-      { _id: userId },
-      { $pull: { posts: postId } },
-      { new: true },
-    )
-    if (!user) {
-      callback(null, (404, `No post found with id: ${userId}, cannot delete`));
-    }
+  // try {
+  //   await connectToDatabase();
+  //   const user = await User.findOneAndUpdate(
+  //     { _id: userId },
+  //     { $pull: { posts: postId } },
+  //     { new: true },
+  //   )
+  //   if (!user) {
+  //     callback(null, (404, `No post found with id: ${userId}, cannot delete`));
+  //   }
 
-    return {
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Allow": "GET, OPTIONS, POST",
-        "Access-Control-Allow-Methods": "GET, OPTIONS, POST",
-        "Access-Control-Allow-Headers": "*"
-      },
-      statusCode: 200,
-      body: JSON.stringify("!!!!!!!!!!!!!!!DONE!!!!!!!!!!!!!!!!!!!"),
-    };
-  } catch (error) {
-    return (error);
-  }
+  //   return {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Access-Control-Allow-Origin": "*",
+  //       "Allow": "GET, OPTIONS, POST",
+  //       "Access-Control-Allow-Methods": "GET, OPTIONS, POST",
+  //       "Access-Control-Allow-Headers": "*"
+  //     },
+  //     statusCode: 200,
+  //     body: JSON.stringify("!!!!!!!!!!!!!!!DONE!!!!!!!!!!!!!!!!!!!"),
+  //   };
+  // } catch (error) {
+  //   return (error);
+  // }
 };
