fbff54c61abb27e3acbeedf43a881ddc7a4e597a
EnrisKumi <107574553+EnrisKumi@users.noreply.github.com>
2022-09-20 13:49:43 +0200

    Pagination for filters

--- handlers/PostHandler/GetPostsByTag.js
+++ handlers/PostHandler/GetPostsByTag.js
@@ -5,12 +5,24 @@ module.exports.getPostsByTag = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
   try {
-    await connectToDatabase();;
+    await connectToDatabase();
     const querystring = event.queryStringParameters;
     let filter = {};
-    filter = querystring.tags.split(',');
+    filter = querystring.tags.split(",");
+    let page = {};
+    page = querystring.page;
+    // console.log(querystring.page);
+    let skipValue = (querystring.page - 1) * 10;
 
-    const post = await Post.find({"tags":filter});
+    let totalDocuments = await Post.count({ "tags": filter });
+    console.log(totalDocuments);
+    totalPages = Math.ceil(totalDocuments / 10);
+    console.log(totalPages);
+
+    const post = await Post.find({ "tags": filter })
+      .skip(skipValue)
+      .limit(10)
+      .sort({ date: -1 });
 
     if (!post) {
       callback(null, (404, "No posts Found."));
