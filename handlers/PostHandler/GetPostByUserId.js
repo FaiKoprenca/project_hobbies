ea8afb6f4056a39588eea8420ad06bb423a8d42c
FaiKoprenca <88925964+FaiKoprenca@users.noreply.github.com>
2022-10-05 14:46:11 +0200

    event status for joined and users posts

--- handlers/PostHandler/GetPostByUserId.js
+++ handlers/PostHandler/GetPostByUserId.js
@@ -10,6 +10,23 @@ module.exports.getPostsByUserId = async (event, context, callback) => {
     try {
       await connectToDatabase();
       const user = await User.findById(id).populate("posts");
+      const post = user.posts
+
+      const dateNow = Math.floor(Date.now() / 1000);
+
+        for (let index = 0; index < post.length; index++) {
+            const { startTime } = post[index];
+            const startTimeU = new Date(startTime)
+            const startTimeUnix = Math.floor(startTimeU.getTime() / 1000);
+            if (startTimeUnix < dateNow) {
+
+                post[index].status = "Completed";
+            }
+            else {
+                post[index].status = "OnGoing";
+            }
+        }
+
   
       if (!user) {
         callback(null, (404, `No user found with id: ${id}`));
--- handlers/PostHandler/GetPostJoined.js
+++ handlers/PostHandler/GetPostJoined.js
@@ -10,6 +10,22 @@ module.exports.getPostJoined = async (event, context, callback) => {
     await connectToDatabase();
     const post = await Post.find({ joined: id }).sort({ date: -1 });
 
+    const dateNow = Math.floor(Date.now() / 1000);
+
+    for (let index = 0; index < post.length; index++) {
+      const { startTime } = post[index];
+      const startTimeU = new Date(startTime)
+      const startTimeUnix = Math.floor(startTimeU.getTime() / 1000);
+      if (startTimeUnix < dateNow) {
+
+        post[index].status = "Completed";
+      }
+      else {
+        post[index].status = "OnGoing";
+      }
+    }
+
+
     if (!post) {
       callback(null, (404, `No post found with id: ${id}`));
     }
